d019cbd5fc9ae8fee0b58aeb6a4f507f7fd96c49
tangerinel <lizaskorohod5@gmail.com>
2023-03-30 10:03:13 +0100

    created book model

--- 3-architecture/3.2-MVC-books-library/src/config/database.ts
+++ 3-architecture/3.2-MVC-books-library/src/config/database.ts
@@ -1,11 +1,9 @@
 import mysql from 'mysql2';
 import {config} from "./config";
 
-const pool = mysql.createPool({
+export default mysql.createPool({
     host: config.server.port,
     user: config.mysql.user,
     password: config.mysql.password,
     database: config.mysql.database,
-  });
-
-module.exports = pool.promise();
+  }).promise();
---
+++ 3-architecture/3.2-MVC-books-library/src/models/book.ts
@@ -0,0 +1,59 @@
+import db from '../config/database';
+
+class Book {
+
+    title: string;
+    authors: string;
+    description: string;
+    year: number;
+    clicks: number;
+
+    constructor (title: string, authors: string,
+         description: string, year:number, clicks: number){
+            this.title = title;
+            this.authors = authors;
+            this.description = description;
+            this.year = year;
+            this.clicks = clicks;
+    }
+
+    readAllBooks(callback){
+        const sql ='SELECT * FROM books';
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            callback(result);
+          });
+    }
+
+    findBookById (id: number, callback){
+        const sql = 'SELECT * FROM books WHERE id = ?';
+        db.query(sql, [id], (err, result)=>{
+            if (err) throw err;
+            callback(result[0]);
+        });
+    }
+
+    save() {
+        const sql = 'INSERT INTO books (title, author, description, year, clicks) VALUES (?, ?, ?, ?, ?)';
+        const values = [this.title, this.authors, this.description, this.year, this.clicks];
+        db.query(sql, values, (err, result) => {
+          if (err) throw err;
+          console.log(`Book ${this.title} saved to database`);
+          return result;
+        });
+    }
+
+    updateBookById(id, callback){
+        const sql =   'UPDATE books SET title = ?, authors = ?, description = ?, year = ?, clicks = ? WHERE id = ?';
+        const values =  [this.title, this.authors, this.description,this.year, this.clicks, id];
+        db.query(sql, values, (err, result) => {
+              if (err) throw err;
+              callback(result.affectedRows);
+            }
+          );
+    }
+
+    deleteBookById (id, callback){
+
+    }
+}
