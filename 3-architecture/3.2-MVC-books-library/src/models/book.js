3610fcd990b144e419aa6d29611cb588a9d5ab9b
tangerinel <lizaskorohod5@gmail.com>
2023-04-07 14:28:38 +0100

    replaced class

--- 3-architecture/3.2-MVC-books-library/src/models/book.js
+++ 3-architecture/3.2-MVC-books-library/src/models/book.js
@@ -1,66 +1,71 @@
 import connection from "../config/connection";
 
-class Book {
-  constructor(title, authors, description, year, clicks, image_url) {
-    this.title = title;
-    this.authors = authors;
-    this.description = description;
-    this.year = year;
-    this.clicks = clicks;
-    this.image_url = image_url;
-  }
-
-  readAllBooks(callback) {
-    const sql = "SELECT * FROM books";
-    connection.query(sql, (err, result) => {
-      if (err) throw err;
-      callback(result);
-    });
-  }
-
-  getBookById(id, callback) {
-    const sql = "SELECT * FROM books WHERE id = ?";
-    connection.query(sql, [id], (err, result) => {
-      if (err) throw err;
-      callback(result[0]);
-    });
-  }
-
-  save() {
-    const sql =
-      "INSERT INTO books (title, author, description, year, clicks, image_url) VALUES (?, ?, ?, ?, ?, ?)";
-    const values = [
-      this.title,
-      this.authors,
-      this.description,
-      this.year,
-      this.clicks,
-      this.image_url,
-    ];
-    connection.query(sql, values, (err, result) => {
-      if (err) throw err;
-      console.log(`Book ${this.title} saved to database`);
-      return result;
-    });
-  }
-
-  updateBookById(id, callback) {
-    const sql =
-      "UPDATE books SET title = ?, authors = ?, description = ?, year = ?, clicks = ?, image_url = ? WHERE id = ?";
-    const values = [
-      this.title,
-      this.authors,
-      this.description,
-      this.year,
-      this.clicks,
-      this.image_url,
-      id,
-    ];
-    connection.query(sql, values, (err, result) => {
-      if (err) throw err;
-      callback(result.affectedRows);
-    });
-  }
-
-  deleteBookById(id, callback) {}
+function readAllBooks(callback) {
+  const sql = "SELECT * FROM books";
+  connection.query(sql, (err, result) => {
+    if (err) console.log(err);
+    callback(result);
+  });
 }
+
+function getBookById(id, callback) {
+  const sql = "SELECT * FROM books WHERE id = ?";
+  connection.query(sql, [id], (err, result) => {
+    if (err) console.log(err);
+    callback(result[0]);
+  });
+}
+
+function getSoftDeletedBooks(callback) {
+  const sql = "SELECT id FROM books WHERE deleted = 1";
+  connection.query(sql, (err, result) => {
+    if (err) console.log(err);
+    callback(result);
+  });
+}
+function saveBook(title, authors, description, year, clicks, image_url) {
+  const sql =
+    "INSERT INTO books (title, author, description, year, clicks, image_url) VALUES (?, ?, ?, ?, ?, ?)";
+  const values = [title, authors, description, year, clicks, image_url];
+  connection.query(sql, values, (err, result) => {
+    if (err) console.log(err);
+    console.log(`Book ${this.title} saved to database`);
+    return result;
+  });
+}
+
+function updateBookById(id, callback) {
+  const sql =
+    "UPDATE books SET title = ?, authors = ?, description = ?, year = ?, clicks = ?, image_url = ? WHERE id = ?";
+  const values = [
+    this.title,
+    this.authors,
+    this.description,
+    this.year,
+    this.clicks,
+    this.image_url,
+    id,
+  ];
+  connection.query(sql, values, (err, result) => {
+    if (err) console.log(err);
+    callback(result.affectedRows);
+  });
+}
+
+function softDeleteBookById(id) {
+  const deleted = 1;
+  const sql = "UPDATE books SET deleted = ? WHERE id = ?";
+  connection.query(sql, [deleted, id], (err, result) => {
+    if (err) console.log(err);
+    console.log(`Book ${this.title} deleted from database`);
+    return result;
+  });
+}
+export {
+  readAllBooks,
+  getBookById,
+  getSoftDeletedBooks,
+  saveBook,
+  softDeleteBookById,
+  updateBookById,
+};
